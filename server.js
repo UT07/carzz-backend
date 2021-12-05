5cc543a0873a3760d1cf493bbaa9fa3a6ee1bc7b
Utkarsh Singh <ut254_7@hotmail.com>
2021-12-04 19:36:20 -0600

    fixed heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT=process.env.PORT||'3001'
 app.listen(PORT,()=>{
     console.log("running server");
 });
-console.log('server started ' + port)
+console.log('server started ' + PORT)
 
 const db=mysql.createConnection({
     user:"root",
